b14efb6e1c20794adcce2f06103be4e80566d5ff
Matthias Jäggli <matthias.jaeggli@gmail.com>
2018-01-24 15:56:38 +0100

    add new version of material-ui

--- groomer/src/App.js
+++ groomer/src/App.js
@@ -1,16 +1,21 @@
 import React, { PureComponent } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class App extends PureComponent {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className='App'>
+          <header className='App-header'>
+            <h1 className='App-title'>Welcome to React</h1>
+          </header>
+          <p className='App-intro'>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <RaisedButton>Click Me</RaisedButton>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
--- groomer/src/index.js
+++ groomer/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import registerServiceWorker from './registerServiceWorker'
 
+injectTapEventPlugin()
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
