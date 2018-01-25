5c032f592059c0ebbf353bf6647e6ba4e191d22b
Matthias Jäggli <matthias.jaeggli@gmail.com>
2018-01-25 02:04:01 +0100

    refresh button

--- groomer/src/Result.js
+++ groomer/src/Result.js
@@ -3,6 +3,7 @@ import { withStyles } from 'material-ui/styles'
 import Card, { CardContent } from 'material-ui/Card'
 import Button from 'material-ui/Button'
 import ArrowBack from 'material-ui-icons/ArrowBack'
+import Autorenew from 'material-ui-icons/Autorenew'
 import Divider from 'material-ui/Divider/Divider'
 import Typography from 'material-ui/Typography/Typography'
 
@@ -30,6 +31,9 @@ const styles = theme => ({
   content2: {
     textAlign: 'center'
   },
+  buttonRefresh: {
+    width: '100%'
+  },
   number1: { backgroundColor: green[500] },
   number2: { backgroundColor: lime[500] },
   number3: { backgroundColor: amber[500] },
@@ -43,34 +47,42 @@ class Result extends React.Component {
     const { classes, score, onAdjust } = this.props
     if (!score) { return 'no score' }
     return (
-      <Card className={classes.card}>
-        <Button
-          className={classes.button}
-          onClick={event => {
-            console.log(onAdjust)
-            onAdjust()
-          }}
-          >
-          <ArrowBack className={classes.leftIcon} />
-          Adjust
-        </Button>
-        <Divider />
-        <CardContent className={classes['number' + score]}>
-          <Typography type='display4' className={classes.content}>
-            {score}
-          </Typography>
-          <Typography className={classes.content}>
-            story point{score > 1 ? 's' : ''}
-          </Typography>
-        </CardContent>
-        {score > 8 &&
-          <CardContent>
-            <Typography type='title' className={classes.content2}>
-              Story should probably be splitted
+      <div>
+        <Card className={classes.card}>
+          <Button
+            className={classes.button}
+            onClick={event => onAdjust()}
+            >
+            <ArrowBack className={classes.leftIcon} />
+            Adjust
+          </Button>
+          <Divider />
+          <CardContent className={classes['number' + score]}>
+            <Typography type='display4' className={classes.content}>
+              {score}
+            </Typography>
+            <Typography className={classes.content}>
+              story point{score > 1 ? 's' : ''}
             </Typography>
           </CardContent>
-        }
-      </Card>
+          {score > 8 &&
+            <CardContent>
+              <Typography type='title' className={classes.content2}>
+                Story should probably be splitted
+              </Typography>
+            </CardContent>
+          }
+        </Card>
+        <Card className={classes.card}>
+          <Button
+            className={classes.buttonRefresh}
+            onClick={event => global.document.location.reload()}
+            >
+            <Autorenew className={classes.leftIcon} />
+            Start over
+          </Button>
+        </Card>
+      </div>
     )
   }
 }
