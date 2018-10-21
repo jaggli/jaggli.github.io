aa3ef8e29903a167979ed4a7a6f169b9a43ad465
Matthias Jäggli <matthias.jaeggli@gmail.com>
2018-10-22 00:01:54 +0200

    correct homepage

--- airplane/service-worker.js
+++ airplane/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28b06c6f9ea5d45decfe348f5793e746.js"
+  "/airplane/precache-manifest.f4e7fc816b6f1b08bb8b20bf2a2dec11.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/airplane/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
